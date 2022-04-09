5525ddc5eef7c4d21caf4f1773eb2901e7af5a37
Kazuhito Miura <kazuhito.sumpic@gmail.com>
2022-04-09 13:59:38 +0900

    first commit.
    ウィザードで新規作成のプロジェクトを作った直後まで。

---
+++ ConnpassAutomator/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConnpassAutomatorAvaloniaUI.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
---
+++ ConnpassAutomator/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace ConnpassAutomatorAvaloniaUI.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
