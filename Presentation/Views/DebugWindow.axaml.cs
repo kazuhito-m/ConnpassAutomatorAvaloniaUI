0abaad29b3b0b1eeaed2c860c93ba52fb05692ce
Kazuhito Miura <kazuhito.sumpic@gmail.com>
2022-05-03 04:41:22 +0900

    debug: 使えるフォント一覧を表示。

--- Presentation/Views/DebugWindow.axaml.cs
+++ Presentation/Views/DebugWindow.axaml.cs
@@ -1,6 +1,8 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using System.Linq;
 
 namespace Presentation.Views
 {
@@ -17,6 +19,14 @@ namespace Presentation.Views
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        private void OnClickGo(object sender, RoutedEventArgs e)
+        {
+            var input = this.FindControl<TextBox>("debugText");
+            var names = System.Drawing.FontFamily.Families
+                .Select(i => i.Name);
+            input.Text = string.Join("\n", names);
         }
     }
 }
